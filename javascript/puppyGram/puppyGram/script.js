14d473e650b440db74c6ac9cab7629872fb1ebd8
thesamcode <stores1@digitalmail.one>
2022-06-28 18:22:49 -0700

    new updates

--- javascript/puppyGram/puppyGram/script.js
+++ javascript/puppyGram/puppyGram/script.js
@@ -29,3 +29,13 @@ function cookieMonster(){
     var element = document.querySelector("#cookie-box");
     element.remove();
 }
+
+function incrementLike(id){
+    console.log("Liking!", id);
+    var element = document.querySelector(id);
+    console.log("element looks like this", element.innerText);
+    let likeCount = Number(element.innerText);
+    console.log(likeCount);
+    likeCount+=1;
+    element.innerText = likeCount;
+}
