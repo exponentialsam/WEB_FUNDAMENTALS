d49fc636d2c3fd706eb7404fc1a98f2913daedbf
thesamcode <stores1@digitalmail.one>
2022-06-30 12:04:39 -0700

    new updates

--- javascript/loop_challenge_assignment/loops.js
+++ javascript/loop_challenge_assignment/loops.js
@@ -1,11 +1,50 @@
 
 
-function odds(){
-    for(var i=0; i<20; i++){
-        if(i%2 == 0){
-            console.log(i);
-        }
-    }
-} 
+// function odds(){
+//     for(var i=0; i<20; i++){
+//         if(i%2 != 0){
+//             console.log(i);
+//         }
+//     }
+// } 
 
-odds();
+// odds();
+
+// function triples(){
+//     for(var i=100; i>-1; i--){
+//         if(i%3 == 0){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// triples();
+
+// function prints(){
+//     var arr = [4,2.5,1,-0.5,-2,-3.5]
+//     for (var i=0; i<arr.length; i++){
+//         console.log(arr[i]);
+//     }
+// }
+
+// prints();
+
+// function summation(){
+//     var sum = 0
+//     for (var i=0; i<=100; i++){
+//         sum = sum + i;
+//     }
+//     console.log(sum);
+// }
+
+// summation();
+
+function factorial(){
+    var product=1
+    for (var i=1; i<=12; i++){
+        product = product*i;
+    }
+    console.log(product);
+}
+
+factorial();
